f134a21f3eb9d7fa0fc1af440df7837fa8ed7beb
tmraaex <tmr.aaex@gmail.com>
2025-12-06 02:34:30 +0100

    config

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from "url";
 import { FileScanner } from "../core/FileScanner.js";
 import { RouteGenerator } from "../core/RouteGenerator.js";
 import path from "path";
+import fs from "fs"
 
 const __filename = fileURLToPath(import.meta.url);
 const pagesDir = "/src/pages";
@@ -11,21 +12,25 @@ async function generateRoutes() {
     const scanner = new FileScanner(pagesDir);
     const fileData = await scanner.get_file_data();
 
-
     // console.log(fileData);
-    
+
+    console.dir(fileData,{depth:null});
 
     const generator = new RouteGenerator();
-    const routeMap = await generator.generateComponentsMap(fileData);
-
+    const routeMap = await generator.generateRoutesFile(fileData);
 
     // console.log("Route map: ", routeMap)
 
-    const routType = await generator.generateRoutesTypeDef(fileData);
+    // const routType = await generator.generateRoutesTypeDef(fileData);
 
-    console.log(routType);
-    
+    // console.log(routeMap);
 
+    fs.writeFile("./src/test/output.ts",routeMap,"utf-8",(err)=>{
+      if (err) {
+        console.log(err);
+        
+      }
+    })
   } catch (error) {
     console.error("Error generating routes:", error);
   }
