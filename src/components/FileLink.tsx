875f336c88111bdc2c72ce54fa8787a7ebf184e8
tmraaex <tmr.aaex@gmail.com>
2025-12-22 02:42:03 +0100

    added active class support

--- src/components/FileLink.tsx
+++ src/components/FileLink.tsx
@@ -1,21 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-interface FileLinkProps<RouteType extends string = string> {
+interface FileLinkProps<RouteType extends string = string>
+  extends React.PropsWithChildren {
   to: RouteType;
-  children: React.ReactNode;
+  activeClassName?: string;
+  className?: string;
 }
 
 export function FileLink<RouteType extends string = string>({
   to,
   children,
+  activeClassName = "active",
+  className = "",
 }: FileLinkProps<RouteType>) {
   const isServer = typeof window === "undefined";
 
   if (isServer) {
-    //supports server side routing 
+    // SSR fallback
     return <a href={to as string}>{children}</a>;
   }
-  // client side routing
-  return <Link to={to as string}>{children}</Link>;
+
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
+  return (
+    <Link
+      to={to as string}
+      className={`${className} ${isActive ? activeClassName : ""}`.trim()}
+    >
+      {children}
+    </Link>
+  );
 }
